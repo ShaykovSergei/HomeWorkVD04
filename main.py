b4fc7d553d99c48db3081590619e1d07fbbb2ec1
Шайков Сергей <shaikoff.serzh@yandex.ru>
2025-11-14 16:54:47 +0300

    add new code

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World!"
+    return render_template("index.html")
 
-@app.route("/new")
-@app.route("/newpage")
-@app.route("/новая страница")
-def new():
-    return "new page"
+
+
+# @app.route("/")
+# def hello_world():
+#     html = """
+#     <h1>тестовый запуск локального сервера</h1>
+#     <p>А это просто текст</p>
+#     """
+#     return html
+
+
+# @app.route("/")
+# @app.route("/<password>/")
+# def hello_world(password=None):
+#     if password == "1234":
+#         return f"Доступ разрешен"
+#     else:
+#         return f"Доступ запрещен"
+
+
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/new")
+@app.route("/newpage")
+@app.route("/новая страница")
+def new():
+    return "new page"
+
+
+if __name__ == "__main__":
+    app.run()
